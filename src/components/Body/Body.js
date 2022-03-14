fa985670a7af7eddedf2114411200fd2828460f7
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2022-03-13 21:16:11 -0500

    minor styling choices

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -9,9 +9,13 @@ function Body(props) {
   //const [name, setName]=useState(null);
   return (
     <div>
+      <nav class="navbar">
+        <span class="navbar-brand mb-0 h1"></span>
+      </nav>
       <div className="Input-Div">
+        <p></p>
         <Card className="Input-Card">
-          <Card.Header className="Input-Header">To Do!</Card.Header>
+          <Card.Header className="Input-Header">To Do List</Card.Header>
           <NewTask getList={props.getList} />
         </Card>
       </div>
